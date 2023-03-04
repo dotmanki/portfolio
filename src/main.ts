10828fb2a2a8ab2b5232ebf30c61419cc45da695
Manki <damianorzusa2016@gmail.com>
2023-03-04 00:32:27 -0300

    design

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
+const onPanelModal = document.getElementById('on-panel-modal')
+const panelModal = document.getElementById('panel-modal')
+const closePanelModal = document.getElementById('close-panel-modal')
+
+const onCvModal = document.getElementById('on-cv-modal')
+const cvModal = document.getElementById('cv-modal')
+const closeCvModal = document.getElementById('close-cv-modal')
+
+const toggleModal = (el: HTMLElement | null) => {
+  el?.setAttribute(
+    'aria-hidden',
+    `${el.ariaHidden === 'true' ? 'false' : 'true'}`
+  )
+
+  el?.classList.toggle('hidden')
+}
+
+onPanelModal?.addEventListener('click', () => toggleModal(panelModal))
+closePanelModal?.addEventListener('click', () => toggleModal(panelModal))
+
+onCvModal?.addEventListener('click', () => toggleModal(cvModal))
+closeCvModal?.addEventListener('click', () => toggleModal(cvModal))
+
 export {}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', './src/**/*.{html,ts}'],
+  content: ['./*.{html,ts}'],
   theme: {
     extend: {
+      colors: {
+        ground: '#34353A',
+        'ground-card': '#3D3E42',
+        primary: '#B694F6',
+        'on-primary': '#fff',
+        gradient: '#F9F871'
+      },
       height: {
         nav: '20vh',
-        header: '80vh'
+        header: '80vh',
+        'md-header': '65vh'
+      },
+      keyframes: {
+        'pulse-shadow': {
+          '0%, 100%': {
+            'box-shadow': '0 4px 6px -1px #B694F6, 0 2px 4px -2px #B694F6'
+          },
+          '50%': {
+            'box-shadow': '0 10px 15px -3px #B694F6, 0 4px 6px -4px #B694F6'
+          }
+        },
+        'gradient-rotate': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' }
+        }
+      },
+      animation: {
+        'pulse-shadow': 'pulse-shadow 2s ease-in-out infinite',
+        'gradient-rotate': 'gradient-rotate 2s ease-in-out infinite'
+      },
+
+      backgroundImage: {
+        'hero-rounded': "url('/hero-rounded.jpg')"
       }
-    },
-    colors: {
-      ground: '#34353A',
-      primary: '#B694F6',
-      'on-primary': '#fff'
     },
     fontFamily: {
       'open-sans': ['Open Sans', 'sans-serif']
     },
-    backgroundImage: {
-      'hero-rounded': "url('/hero-rounded.jpg')"
+    flex: {
+      '0-0-a': '0 0 auto'
     }
   },
   plugins: []
